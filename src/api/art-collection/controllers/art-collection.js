ba184fc85c22252a941f4535122b4086e3854986
Paul Matthew <116613750+paul-matthew@users.noreply.github.com>
2023-06-11 20:29:57 -0400

    first commit

---
+++ src/api/art-collection/controllers/art-collection.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * art-collection controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::art-collection.art-collection');
---
+++ src/api/art-collection/routes/art-collection.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * art-collection router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::art-collection.art-collection');
