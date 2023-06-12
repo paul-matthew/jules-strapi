5b804b915d458ef163ad06d976f4bb9e0fbd770d
Paul Matthew <116613750+paul-matthew@users.noreply.github.com>
2023-06-12 16:39:08 -0400

    Baseline 1.10

--- config/admin.js
+++ config/admin.js
@@ -5,7 +5,7 @@ module.exports = ({ env }) => ({
     secret: env('ADMIN_JWT_SECRET'),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: env('API_TOKEN_SALT') || "G5CnmhGgFCk/WpTMyIfPzA==",
   },
   transfer: {
     token: {
